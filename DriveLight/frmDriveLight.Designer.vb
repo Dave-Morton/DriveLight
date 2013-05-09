a4e835b64f597b7167059fecfa363f8b1c894e55
Dave Morton <dmorton@geekcavecreations.com>
2013-05-08 23:33:19 -0700

    Initial Release
    Please see the README file for more information

---
+++ DriveLight/frmDriveLight.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Assembly: CLSCompliant(False)> 
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDriveLight
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId:="System.Windows.Forms.ToolStripItem.set_Text(System.String)")> <System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId:="System.Windows.Forms.NotifyIcon.set_Text(System.String)")> <System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId:="System.Windows.Forms.Form.set_Text(System.String)")> <System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId:="System.Windows.Forms.Control.set_Text(System.String)")> <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormDriveLight))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.TSM_About = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TSM_Exit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PerformanceCounter1 = New System.Diagnostics.PerformanceCounter()
+        Me.PerformanceCounter2 = New System.Diagnostics.PerformanceCounter()
+        Me.TSM_DL = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.PerformanceCounter1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PerformanceCounter2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Drive Light" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Right-Click to exit"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSM_DL, Me.ToolStripSeparator1, Me.TSM_About, Me.TSM_Exit})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.ShowImageMargin = False
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(131, 82)
+        Me.ContextMenuStrip1.Text = "Drive Light"
+        '
+        'TSM_About
+        '
+        Me.TSM_About.Name = "TSM_About"
+        Me.TSM_About.Size = New System.Drawing.Size(130, 24)
+        Me.TSM_About.Text = "About..."
+        '
+        'TSM_Exit
+        '
+        Me.TSM_Exit.Name = "TSM_Exit"
+        Me.TSM_Exit.Size = New System.Drawing.Size(130, 24)
+        Me.TSM_Exit.Text = "Exit"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'PerformanceCounter1
+        '
+        Me.PerformanceCounter1.CategoryName = "LogicalDisk"
+        Me.PerformanceCounter1.CounterName = "Disk Read Bytes/sec"
+        Me.PerformanceCounter1.InstanceName = "_Total"
+        '
+        'PerformanceCounter2
+        '
+        Me.PerformanceCounter2.CategoryName = "LogicalDisk"
+        Me.PerformanceCounter2.CounterName = "Disk Write Bytes/sec"
+        Me.PerformanceCounter2.InstanceName = "_Total"
+        '
+        'TSM_DL
+        '
+        Me.TSM_DL.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TSM_DL.Name = "TSM_DL"
+        Me.TSM_DL.Size = New System.Drawing.Size(130, 24)
+        Me.TSM_DL.Text = "Drive Light"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(127, 6)
+        '
+        'frmDriveLight
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(160, 56)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmDriveLight"
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Drive Light"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.PerformanceCounter1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PerformanceCounter2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PerformanceCounter1 As System.Diagnostics.PerformanceCounter
+    Friend WithEvents PerformanceCounter2 As System.Diagnostics.PerformanceCounter
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents TSM_About As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TSM_Exit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TSM_DL As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+
+End Class
---
+++ DriveLight/frmDriveLight.vb
@@ -0,0 +1,56 @@
+﻿Public Class FormDriveLight
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim both As Integer = 0
+        If (PerformanceCounter1.NextValue > 0) Then
+            both += 1
+        End If
+
+        If (PerformanceCounter2.NextValue > 0) Then
+            both += 2
+        End If
+
+        Select Case both
+            Case 0
+                NotifyIcon1.Icon = My.Resources.DA_Trans
+            Case 1
+                NotifyIcon1.Icon = My.Resources.DA_Green
+            Case 2
+                NotifyIcon1.Icon = My.Resources.DA_Red
+            Case 3
+                NotifyIcon1.Icon = My.Resources.DA_Both
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        NotifyIcon1.Icon = My.Resources.DA_Green
+        'Dim PerformanceCounter1 As New PerformanceCounter, PerformanceCounter2 As New PerformanceCounter
+
+        With PerformanceCounter1
+            .CategoryName = "LogicalDisk"
+            .InstanceName = "_Total"
+            .CounterName = "Disk Read Bytes/sec"
+        End With
+
+        With PerformanceCounter2
+            .CategoryName = "LogicalDisk"
+            .InstanceName = "_Total"
+            .CounterName = "Disk Write Bytes/sec"
+        End With
+    End Sub
+
+    Private Sub NotifyIcon1_MouseClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseClick
+        'If e.Button = Windows.Forms.MouseButtons.Right Then
+        'NotifyIcon1.Visible = False
+        'Application.Exit()
+        'End If
+    End Sub
+
+    Private Sub TSM_About_Click(sender As System.Object, e As System.EventArgs) Handles TSM_About.Click
+        AboutBox1.Show()
+    End Sub
+
+    Private Sub TSM_Exit_Click(sender As Object, e As System.EventArgs) Handles TSM_Exit.Click
+        Application.Exit()
+    End Sub
+End Class
